1004734e8e63f9fac87195298277787f5be875d5
nickwetter <nickwetter99@gmail.com>
2020-03-18 21:18:07 -0700

    All webdriver properties set, instance creation working

--- src/main/java/com/nickwetter/Main.java
+++ src/main/java/com/nickwetter/Main.java
@@ -1,10 +1,25 @@
 package com.nickwetter;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.safari.SafariDriver;
+
 public class Main
 {
 
+
     public static void main(String[] args)
     {
+        System.setProperty("webdriver.gecko.driver", "webdrivers/geckodriver");
+        System.setProperty("webdriver.chrome.driver", "webdrivers/chromedriver");
+        System.setProperty("webdriver.safari.driver", "webdrivers/safaridriver");
+
+
+        WebDriver ffdriver = new FirefoxDriver();
+        WebDriver gcdriver = new ChromeDriver();
+        WebDriver sdriver = new SafariDriver();
 
     }
 }
